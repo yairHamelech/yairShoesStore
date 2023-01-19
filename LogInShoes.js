42413d11a75beb4ab6a88c1ce0fe543f3bcbb253
yairHamelech <121726339+yairHamelech@users.noreply.github.com>
2023-01-19 20:21:36 +0200

    ךללחיע

--- LogInShoes.js
+++ LogInShoes.js
@@ -49,11 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll(".form__input").forEach((inputElement) => {
     inputElement.addEventListener("blur", (e) => {
-      if (
-        e.target.id === "signupUsername" &&
-        e.target.value.length > 0 &&
-        e.target.value.length < 10
-      ) {
+      if (e.target.id === "signupUsername" && e.target.value.length < 10) {
         setInputError(
           inputElement,
           "Username must be at least 10 characters in length"
