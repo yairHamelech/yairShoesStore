1fe3591884ebcdd189ac8bd318fa7294da2a54a9
yairHamelech <121726339+yairHamelech@users.noreply.github.com>
2023-01-28 21:34:10 +0200

    m,hggfs

--- LogInShoes.js
+++ LogInShoes.js
@@ -1,28 +1,3 @@
-function setFormMessage(formElement, type, message) {
-  const messageElement = formElement.querySelector(".form__message");
-
-  messageElement.textContent = message;
-  messageElement.classList.remove(
-    "form__message--success",
-    "form__message--error"
-  );
-  messageElement.classList.add(`form__message--${type}`);
-}
-
-function setInputError(inputElement, message) {
-  inputElement.classList.add("form__input--error");
-  inputElement.parentElement.querySelector(
-    ".form__input-error-message"
-  ).textContent = message;
-}
-
-function clearInputError(inputElement) {
-  inputElement.classList.remove("form__input--error");
-  inputElement.parentElement.querySelector(
-    ".form__input-error-message"
-  ).textContent = "";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
@@ -45,20 +20,5 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
 
     setFormMessage(loginForm, "error", "Invalid username/password combination");
-  });
-
-  document.querySelectorAll(".form__input").forEach((inputElement) => {
-    inputElement.addEventListener("blur", (e) => {
-      if (e.target.id === "signupUsername" && e.target.value.length < 10) {
-        setInputError(
-          inputElement,
-          "Username must be at least 10 characters in length"
-        );
-      }
-    });
-
-    inputElement.addEventListener("input", (e) => {
-      clearInputError(inputElement);
-    });
   });
 });
--- buyingCart.js
+++ buyingCart.js
@@ -1,15 +1,4 @@
-function render(shopingList) {
-  console.log(shopingList);
-  const shopingCart = document.getElementById("shopping-cart");
-  console.log(shopingCart);
-  shopingCart.innerHTML = "";
-
-  for (let index = 0; index < shopingList.length; index++) {
-    const name = document.createElement("p");
-    name.innerText = shopingList[index].name;
-    shopingCart.appendChild(name);
-  }
-}
-
 const shoes = JSON.parse(localStorage.getItem("shoes"));
-render(shoes);
+const currentShoes = shoes.filter((shoes) => shoes.isSelected);
+
+createShoes(currentShoes);
