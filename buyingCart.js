2ec7dcd9bb29999326cbc692bbb7531988b23b46
yairHamelech <121726339+yairHamelech@users.noreply.github.com>
2023-01-31 22:13:17 +0200

    wowwow

--- buyingCart.js
+++ buyingCart.js
@@ -1,5 +1,6 @@
 const shoes = JSON.parse(localStorage.getItem("shoes"));
 const currentShoes = shoes.filter((shoes) => shoes.isSelected);
+let clickCount = 0;
 
 createShoes(currentShoes);
 // function buy(buy) {
@@ -14,11 +15,19 @@ let count = 0;
 for (let i = 0; i < currentShoes.length; i++) {
   count++;
 }
+let warning = document.getElementById("warning");
+warning.innerText =
+  "you have nothing on your shopping cart, please add to cart a shoe first";
+warning.style.display = "none";
 
-document.querySelector(".btn").addEventListener("click", function (e) {
-  const warning = document.createElement("p");
-  warning.classList.add("warning");
-  if (e.target.innerText == "buy now" && count == 0) {
-    e.target.innerText = "continue shopping";
+document.querySelector(".btn").addEventListener("click", function (btn) {
+  if (btn.target.innerText == "buy now" && count == 0) {
+    btn.target.innerText = "continue shopping";
+
+    warning.style.display = "inline-block";
+  }
+  clickCount++;
+  if (clickCount > 1 && count == 0) {
+    window.location.assign("index.html");
   }
 });
